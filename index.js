b6722617c25d0f02508d751b3b85716480276e75
Jiyashah26 <jiyashah6726@gmail.com>
2025-01-13 20:41:34 +0530

    Edit and save data in backend

--- index.js
+++ index.js
@@ -1,63 +1,102 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-const profiles= [
-    {
-        id:1,
-        insta: "Tehseen-student",
-        name: "Tehseen",
-        desc: "student",
-       
-
-    },
-    {
-        id:2,
-        insta: "Nawaf-student",
-        name: "Nawaf",
-        desc: "student",
-        github: 'Sknawaf05'
-
-    },
-    {
-        id:3,
-        insta: "moin-student",
-        name: "Moin",
-        desc: "student",
-
-    },
-    {
-        id:4,
-        insta: "Basheer-student",
-        name: "Basheer",
-        desc: "student",
-
-    }
-   ] 
+const profiles = [
+  {
+    id: 1,
+    link: "Tehseen-student",
+    name: "Tehseen",
+    desc: "student",
+  },
+  {
+    id: 2,
+    link: "Nawaf-student",
+    name: "Nawaf",
+    desc: "student",
+  },
+  {
+    id: 3,
+    link: "moin-student",
+    name: "Moin",
+    desc: "student",
+  },
+  {
+    id: 4,
+    link: "Basheer-student",
+    name: "Basheer",
+    desc: "student",
+  },
+];
 
 // / => http://basheer.com -> endpoint / api
-app.get('/',(req,res)=>{
-    res.send("Test 9")
+app.get("/", (req, res) => {
+  res.send("Test 9");
+});
 
+app.get("/profiles", (req, res) => {
+  res.status(200).json(profiles);
+});
 
-})
+app.get("/profile/:id", (req, res) => {
+  const id = req.params.id;
+  const profile = profiles.find((profile) => {
+    return profile.id == id;
+  });
 
-app.get('/profiles', (req,res)=>{
-    res.status(200).json(profiles);
-}) 
+  res.send(profile ?? "Not found");
+});
 
-app.get('/profile/:id',(req,res)=>{
-   const id = req.params.id;
-   const profile = profiles. find((profile)=>{
-    return profile.id == id
-   })
-    
-    res.send(profile??"Not found")
-})
+//create new profile
 
-app.listen('8000',()=>{
-    console.log("Server is running on port 8000...")
-})
+app.post("/profileCreate/", (req, res) => {
+  const profile = req.body;
+  profiles.push(profile);
+  res.status(200).json(profiles);
+});
 
+app.put("/profileEdit/", (req, res) => {
+
+  // Algorithm
+  // step1: Save req data in profile variable
+  // step2: Find index
+  // step3: Check index is there or not
+  // step4: Replace the new value
+  // step5: Return updated value
+
+  // save req data in profile variable
+  const profile = req.body;
+
+  // to find index
+  const profileIndex = profiles.findIndex((item) => {
+    return item.id == profile.id;
+  });
+
+  // check index is there or not
+  if(profileIndex == -1){
+      res.status(404).send("Not found");
+  }
+
+  // replace the new value
+  profiles[profileIndex] = profile;
+
+  // return updated value
+  res.status(200).send(profiles);
+  
+//   const profile = req.body;
+//   const newProfiles = profiles.map((item) => {
+//     if(item.id == profile.id){
+//         return { ...profile };
+//     }else{
+//         return { ...item };
+//     }
+//   });
+//   res.status(200).send(newProfiles);
+});
+
+app.listen("8000", () => {
+  console.log("Server is running on port 8000...");
+});
